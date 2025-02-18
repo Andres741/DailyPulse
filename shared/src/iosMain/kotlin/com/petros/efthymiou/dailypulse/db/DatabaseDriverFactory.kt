4e86d38012c85107c53f44055f2dfe199162964b
andres <aconderodriguez272@gmail.com>
2025-02-18 12:50:01 +0100

    fix gradle

--- shared/src/iosMain/kotlin/com/petros/efthymiou/dailypulse/db/DatabaseDriverFactory.kt
+++ shared/src/iosMain/kotlin/com/petros/efthymiou/dailypulse/db/DatabaseDriverFactory.kt
@@ -4,7 +4,7 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import petros.efthymiou.dailypulse.db.DailyPulseDatabase
 
-actual class DatabaseDriverFactory() {
+actual class DatabaseDriverFactory {
 
     actual fun createDriver(): SqlDriver =
         NativeSqliteDriver(
